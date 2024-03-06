01f2979a2078bb511813b0d6802345b140afa477
Marcus Bugge <marcusbugge@Marcuss-MacBook-Air.local>
2024-03-06 17:45:47 +0100

    removal of =

--- src/app/api/gamer-proxy/route.ts
+++ src/app/api/gamer-proxy/route.ts
@@ -1,27 +1,37 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-  const gamerUrlWithQueryParams = req.url.substring(req.url.indexOf("?") + 1);
+  let gamerUrlWithQueryParams = req.url.substring(req.url.indexOf("?") + 1);
 
   // Decode the URL
-  const decodedUrl = decodeURIComponent(gamerUrlWithQueryParams);
+  let decodedUrl = decodeURIComponent(gamerUrlWithQueryParams);
+
+  // Ensure the URL does not end with "="
+  if (decodedUrl.endsWith("=")) {
+    decodedUrl = decodedUrl.slice(0, -1);
+  }
 
   console.log("url", decodedUrl);
 
-  const fetchResponse = await fetch(decodedUrl, {
-    method: "GET", // Optional, as GET is the default method
-    headers: {
-      Authorization: `Bearer ${process.env.GAMER_API_TOKEN}`,
-      Accept: "application/json",
-    },
-  });
+  try {
+    const fetchResponse = await fetch(decodedUrl, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.GAMER_API_TOKEN}`,
+        Accept: "application/json",
+      },
+    });
 
-  if (!fetchResponse.ok) {
-    // Handle the error according to your needs
-    throw new Error(`Error fetching data: ${fetchResponse.statusText}`);
+    if (!fetchResponse.ok) {
+      // Handle the error according to your needs
+      throw new Error(`Error fetching data: ${fetchResponse.statusText}`);
+    }
+
+    const responseData = await fetchResponse.json();
+    return NextResponse.json(responseData);
+  } catch (error) {
+    console.error("Fetch error:", error);
+    // Handle or throw the error appropriately
+    throw new Error("Error in fetching data");
   }
-
-  const responseData = await fetchResponse.json();
-
-  return NextResponse.json(responseData);
 }
